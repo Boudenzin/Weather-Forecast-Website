73dd965cfb16b789e0f417acd7bdf4a6f1d93d20
romildo.rodrigues <romildo-rrj@hotmail.com>
2024-11-27 11:17:20 -0300

    Adição do CSS do WeatherChart

--- weather-forecast/src/App.js
+++ weather-forecast/src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
+const FORECAST_URL = 'https://api.openweathermap.org/data/2.5/forecast';
 
 function capitalizarDescricao(description) {
   return description
@@ -20,34 +21,39 @@ function App() {
 
 
   const [dadosDoClima, setDadosDoClima] = useState(null);
+  const [dadosPrevisao, setDadosPrevisao] = useState(null);
 
   
   const handleCitySearch = async(city) => {
     try {
       console.log('Buscando a previsão do tempo para: ${city}');
-      const response = await fetch(`${BASE_URL}?q=${city}&appid=${API_KEY}&units=metric&lang=pt_br`); //Deixa em graus ceulsius e em br
-      const dado = await response.json();
+      const weatherResponse = await fetch(`${BASE_URL}?q=${city}&appid=${API_KEY}&units=metric&lang=pt_br`); //Deixa em graus ceulsius e em br
+      const dadoAtual = await weatherResponse.json();
 
-      if (dado.cod === 200) {
+      if (dadoAtual.cod === 200) {
 
         const horaAtual = Date.now() / 1000; //Hora atual em segundos
 
         //Determina se é dia ou noite
 
-        const ehDeDia = horaAtual >= dado.sys.sunrise && horaAtual < dado.sys.sunset;
+        const ehDeDia = horaAtual >= dadoAtual.sys.sunrise && horaAtual < dadoAtual.sys.sunset;
 
-        const weatherIcon = getWeatherIcon(dado.weather[0].description, ehDeDia);
+        const weatherIcon = getWeatherIcon(dadoAtual.weather[0].description, ehDeDia);
 
         setDadosDoClima({
-          cidade: dado.name,
-          temperatura: Math.round(dado.main.temp),
-          descricao: capitalizarDescricao(dado.weather[0].description),
-          vento: `${dado.wind.speed} m/s`,
-          umidade: `${dado.main.humidity}%`,
+          cidade: dadoAtual.name,
+          temperatura: Math.round(dadoAtual.main.temp),
+          descricao: capitalizarDescricao(dadoAtual.weather[0].description),
+          vento: `${dadoAtual.wind.speed} m/s`,
+          umidade: `${dadoAtual.main.humidity}%`,
           icon: weatherIcon,
           ehDeDia
         });
-      
+
+        //Fetch para prever os proximos horarios
+        const forecastResponse = await fetch(`${FORECAST_URL}?q=${city}&appid=${API_KEY}&units=metric&lang=pt_br`);
+        const dadoFuturo = await forecastResponse.json();
+        setDadosPrevisao(dadoFuturo);
     } else {
       alert('Cidade não encontrada');
     }
@@ -86,6 +92,8 @@ function App() {
         />
         )}
       </div>
+      {/* Rendereização do gráfico de previsão */}
+      {dadosPrevisao && <WeatherChart forecastData={dadosPrevisao} />}
     </div>
   );
 }
